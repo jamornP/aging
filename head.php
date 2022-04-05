442e2fe231d6abb036d60a0cde58eec4fb55907a
jamornP <jamorn07@gmail.com>
2022-04-05 21:30:20 +0700

    header

---
+++ head.php
@@ -0,0 +1,13 @@
+<?php
+    echo "
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>Comprehensive Geriatric Assessment</title>
+            <link rel='icon' href='./img/favicon.ico' type=''>
+        </head>
+    ";
+?>
